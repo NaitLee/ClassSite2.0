63124ba7682127533ae17fe79d0d844f95c8bfc2
NaitLee <47078558+NaitLee@users.noreply.github.com>
2020-05-09 11:47:12 +0800

    New object structures

--- js/inititems.js
+++ js/inititems.js
@@ -1,51 +1,32 @@
 // Add items in the mainpage item list.
 
-function item(data) {
-    this.nameA = data.nameA;
-    this.nameB = data.nameB;
-    this.link = data.link;
-    this.description = data.discription;
+function item(nameA='', nameB='', link='/', description='') {
+    this.nameA = nameA;
+    this.nameB = nameB;
+    this.link = link;
+    this.description = description;
 }
 
 var count = 0;
 function isOdd(num) {
     return num%2==1 ? true : false;
 }
-var items = [{
-        nameA: '各科课件',
-        nameB: 'Coursewares',
-        link: '/coursewares/',
-        description: '查看收集的各科课件及资料'
-    }, {
-        nameA: '上传文件',
-        nameB: 'Upload',
-        link: '/uploads/',
-        description: '上传并分享一些文件吧'
-    }, {
-        nameA: '作品',
-        nameB: 'Projects',
-        link: '/projects/',
-        description: '大家的文学作品、艺术作品，可供欣赏、借鉴'
-    }, {
-        nameA: '照片与视频',
-        nameB: 'Photos & Videos',
-        link: '/media/',
-        description: '一览曾经的美妙时光'
-    }, {
-        nameA: '音乐',
-        nameB: 'Music & Songs',
-        link: '/music/',
-        description: '舒缓心情，平和心境'
-    }
+
+var items = [
+    new item('各科课件',    'Coursewares',      '/coursewares/',    '查看收集的各科课件及资料'),
+    new item('上传文件',    'Upload',           '/uploads/',        '上传并分享一些文件吧'),
+    new item('作品',        'Projects',         '/projects/',       '大家的文学作品、艺术作品，可供欣赏、借鉴'),
+    new item('照片与视频',  'Photos & Videos',  '/media/',          '一览曾经的美妙时光'),
+    new item('音乐',        'Music & Songs',    '/music/',          '舒缓心情，平和心境')
 ];
 
 
-items.forEach(function (j) {
+items.forEach(function (item) {
     direction = isOdd(++count) ? 'left' : 'right';
     itemlist.innerHTML += '\
         <div class="item '+direction+'">\
-        <a href="'+j.link+'">'+j.nameA+' - '+j.nameB+'<span class="item arrow"></span></a>\
-        <div class="item '+direction+' description">'+j.description+'</div>\
+        <a href="'+item.link+'">'+item.nameA+' - '+item.nameB+'<span class="item arrow"></span></a>\
+        <div class="item '+direction+' description">'+item.description+'</div>\
         <div class="item '+direction+' border"></div>\
         </div>\
     ';
--- js/initnav.js
+++ js/initnav.js
@@ -1,43 +1,24 @@
 // Add navs in the mainpage nav bar.
 
+function navitem (nameA='', link='/', target='', icon='') {
+    this.nameA = nameA;
+    this.link = link;
+    this.target = target;
+    this.icon = icon;
+}
+
 var count = 0;
-var navs = [{
-        nameA: '',
-        link: '/',
-        target: ''
-    }, {
-        nameA: '课件&资料',
-        link: '/coursewares/',
-        target: ''
-    }, {
-        nameA: '上传文件',
-        link: '/uploads/',
-        target: ''
-    }, {
-        nameA: '班级公务',
-        link: '/classworks/',
-        target: ''
-    }, {
-        nameA: '服务器目录',
-        link: '/root/',
-        target: ''
-    }, {
-        nameA: '管理员登录',
-        link: '/~login',
-        target: ''
-    }, {
-        nameA: '<img src="/pic/deco/dingtalk.ico" class="navicon" /> 钉钉网页版',
-        link: 'https://im.dingtalk.com/',
-        target: '_blank'
-    }, {
-        nameA: '<img src="/pic/deco/zhiying.png" class="navicon" /> 智鹰阅卷',
-        link: 'http://app.jouletek.com:8080/panguan/index.html#/',
-        target: '_blank'
-    }
+var navs = [
+    new navitem('课件与资料',   '/coursewares/'),
+    new navitem('上传文件',     '/uploads/'),
+    new navitem('班级公务',     '/classworks/'),
+    new navitem('服务器目录',   '/root/'),
+    new navitem('管理员登录',   '/~signin'),
+    new navitem('钉钉网页版',   'https://im.dingtalk.com/', '_blank', '/pic/deco/dingtalk.ico'),
+    new navitem('智鹰阅卷',     'http://app.jouletek.com:8080/panguan/index.html#/', '_blank', '/pic/deco/zhiying.png')
 ];
 
-navs.forEach(function (j) {
+navs.forEach(function (item, index) {
     nav.innerHTML += '\
-        <li><a href="'+j.link+'" target="'+j.target+'">'+j.nameA+'</a></li>\
-    ';
+        <li><a href="'+item.link+'" target="'+item.target+'"><img src="'+item.icon+'" class="navicon" /> '+item.nameA+'</a></li>\    ';
 });
